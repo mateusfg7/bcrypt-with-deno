12a24ea9eba3ba109e807af4b6c5fc52accd987e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-04-13 10:57:03 -0300

    create User model

---
+++ models/User.ts
@@ -0,0 +1,19 @@
+import { DataTypes, Model } from "https://deno.land/x/denodb/mod.ts";
+
+class User extends Model {
+  static table = "users";
+  static timestamps = true;
+
+  static fields = {
+    id: {
+      type: DataTypes.UUID,
+      unique: true,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password_hash: DataTypes.STRING,
+  };
+}
+
+export default User;
