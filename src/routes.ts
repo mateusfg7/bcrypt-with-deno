ab69f822982287247c245882eea31852262fbe9b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-04-14 10:21:04 -0300

    create /signup route

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,64 @@
-import { Router } from "https://deno.land/x/oak/mod.ts";
+import { Router } from 'https://deno.land/x/oak/mod.ts';
 
-import database from "./database/connection.ts";
+import database from './database/connection.ts';
 
-import User from "./models/User.ts";
+import User from './models/User.ts';
 
 database.link([User]);
 database.sync();
 
 const router = new Router();
 
-router.get("/", (context) => {
-  context.response.body = { message: "Hello Word!" };
+interface CreateUserRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+router.get('/', (context) => {
+  context.response.body = { message: 'Hello Word!' };
+});
+
+router.post('/signup', async (context) => {
+  const requestBody = context.request.body();
+
+  if (requestBody.type != 'json') {
+    context.response.status = 400;
+    context.response.body = { error: 'Request body needs to be a JSON' };
+    return;
+  }
+
+  const { name, email, password }: CreateUserRequest = await requestBody.value;
+
+  try {
+    const listOfAccountsWithEmail = await User.where('email', email).all();
+
+    if (listOfAccountsWithEmail.length != 0) {
+      context.response.status = 400;
+      context.response.body = {
+        error: 'This email has already been registered',
+      };
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+    context.response.status = 500;
+  }
+
+  try {
+    await User.create([
+      {
+        name,
+        email,
+        password_hash: password,
+      },
+    ]);
+
+    context.response.status = 201;
+  } catch (err) {
+    console.log(err);
+    context.response.status = 500;
+  }
 });
 
 export default router;
