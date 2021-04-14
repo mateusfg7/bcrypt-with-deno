0e30faaba785de21fc2b809969e9f61b98d26ef9
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-04-14 13:24:42 -0300

    create /signin route

--- src/routes.ts
+++ src/routes.ts
@@ -14,6 +14,10 @@ interface CreateUserRequest {
   email: string;
   password: string;
 }
+interface LoginUserRequest {
+  email: string;
+  password: string;
+}
 
 router.get('/', (context) => {
   context.response.body = { message: 'Hello Word!' };
@@ -58,6 +62,47 @@ router.post('/signup', async (context) => {
   } catch (err) {
     console.log(err);
     context.response.status = 500;
+  }
+});
+
+router.post('/signin', async (context) => {
+  const requestBody = context.request.body();
+
+  if (requestBody.type != 'json') {
+    context.response.status = 400;
+    context.response.body = { error: 'Request body needs to be a JSON' };
+    return;
+  }
+
+  const { email, password }: LoginUserRequest = await requestBody.value;
+
+  try {
+    const listOfAccountsWithEmail = await User.where('email', email).all();
+
+    if (listOfAccountsWithEmail.length == 0) {
+      context.response.status = 403;
+      context.response.body = {
+        error: 'This account has not been registered',
+      };
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+    context.response.status = 500;
+  }
+
+  const account = await User.where('email', email).first();
+
+  if (account.password_hash == password) {
+    context.response.status = 200;
+    context.response.body = {
+      id: account.id,
+      name: account.name,
+      email: account.email,
+    };
+  } else {
+    context.response.status = 401;
+    context.response.body = { error: 'Password Incorrect' };
   }
 });
 
