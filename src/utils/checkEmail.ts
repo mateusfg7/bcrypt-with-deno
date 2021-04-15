0814632ac64f78ebbcb23f19cb516bd1fc198f40
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-04-15 10:32:19 -0300

    remove console.log on checkEmail util

--- src/utils/checkEmail.ts
+++ src/utils/checkEmail.ts
@@ -3,8 +3,6 @@ import User from '../models/User.ts';
 async function checkIfEmailExists(email: string) {
   const listOfAccountsWithEmail = await User.where('email', email).all();
 
-  console.log(listOfAccountsWithEmail);
-
   if (listOfAccountsWithEmail.length == 0) {
     return false;
   }
